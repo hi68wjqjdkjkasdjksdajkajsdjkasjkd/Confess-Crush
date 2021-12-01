2840c2bc981aafc02d6bf7923fa1eeae9891e12c
hi68wjqjdkjkasdjksdajkajsdjkasjkd <95350017+hi68wjqjdkjkasdjksdajkajsdjkasjkd@users.noreply.github.com>
2021-12-01 18:25:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Gửi cho .......<3",
+    introTitle: 'Nguyễn Kim ANh',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
@@ -9,12 +9,12 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    btnNo: 'Mơ đi liu liu',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu anh ko cầng lý doooo',
+    mess: 'ANh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Mau khỏe nhaaaaaaa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: https://www.facebook.com/buileminh2811
 }
